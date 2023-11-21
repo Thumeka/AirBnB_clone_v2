fb7d82beecfbcbfbcf05ca45b254617e92687c00
thumeka <thumekatembani21@gmail.com>
2023-11-21 13:14:45 -0800

    trying

--- models/city.py
+++ models/city.py
@@ -12,9 +12,5 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-<<<<<<< HEAD
-    places = relationship('Place', cascade='all, delete', backref='cities')
-=======
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref='cities', cascade='all, delete, delete-orphan')
->>>>>>> 51d937ac3739ff8e0036437a77c3c1c646846a48
+    places = relationship("Place", cascade="all, delete", backref="user")
