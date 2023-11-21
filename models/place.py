605ed4f6f222fb18ddcd003b627786240ba6b1b1
Joumana <joumanasalahedin@gmail.com>
2023-11-21 20:29:53 +0100

    task 9 and review class update

--- models/place.py
+++ models/place.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
+import shlex
 
 
 class Place(BaseModel, Base):
@@ -16,6 +21,26 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='delete', backref="place")
+
+    else:
+        @property
+        def reviews(self):
+            """getter for reviews"""
+            var = models.storage.all()
+            review_list = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    review_list.append(var[key])
+            for elem in review_list:
+                if (elem.place_id == self.id):
+                    result.append(elem)
+            return (result)
