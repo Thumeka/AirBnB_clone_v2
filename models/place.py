cb3f7013dd9a441a7db50e5170bee36f5ef9dc62
Joumana <joumanasalahedin@gmail.com>
2023-11-22 17:55:35 +0100

    task 10

--- models/place.py
+++ models/place.py
@@ -8,6 +8,14 @@ from os import getenv
 import models
 import shlex
 
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -27,6 +35,8 @@ class Place(BaseModel, Base):
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade='delete', backref="place")
+        amenities = relationship(
+            'Amenity', secondary=place_amenity, viewonly=False, back_populates='place_amenities')
 
     else:
         @property
@@ -44,3 +54,14 @@ class Place(BaseModel, Base):
                 if (elem.place_id == self.id):
                     result.append(elem)
             return (result)
+
+        @property
+        def amenities(self):
+            """getter for amenities"""
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """Appends amenity ids to attribute"""
+            if type(obj) is models.Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
