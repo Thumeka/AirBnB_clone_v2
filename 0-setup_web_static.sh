bbfd9442d680e959cdb05b0ede6d9f0751c3a0c9
thumeka <thumekatembani21@gmail.com>
2023-12-07 17:04:26 -0800

    k

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,40 @@
 #!/usr/bin/env bash
-# sets up web servers for web_static deployment
+# Sets up the web server for the deployment of web_static
 
+# Install Nginx if not already installed
 sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+sudo apt-get install -y nginx
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
+#Create the folder /data/web_static/releases/ if it doesn’t already exist
 sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
 
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+#Create the folder /data/web_static/shared/ if it doesn’t already exist
+sudo mkdir -p /data/web_static/shared/
+
+#Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+sudo mkdir /data/web_static/releases/test
+
+#Create a fake HTML file /data/web_static/releases/test/index.html w/ simple content
+echo "<html>
   <head>
-    <title>Holberton School</title>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" | sudo tee  /data/web_static/releases/test/index.html
 
+#Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
+#If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data/
+#Give recursive ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -hR ubuntu:ubuntu /data/
 
-sudo sed -i '/server {/a \\tlocation \/hbnb_static\/ { alias \/data\/web_static\/current\/; index index.html; }' /etc/nginx/sites-enabled/default
+#Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+#sudo sed -i '7i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+if [[ $(grep -c "location \/hbnb_static\/" /etc/nginx/sites-enabled/default) -eq 0 ]]
+then
+        sed -i "s/^\}$/\tlocation \/hbnb_static\/ \{\n\t\talias \/data\/web_static\/current\/\;\n\t\}\n\}/" /etc/nginx/sites-enabled/default
+fi
 
-sudo service nginx restart
+sudo service nginx start
